5acba5f22cb524c1262fb2666c6f313d85d0fbe8
Paweena Chinasri <65160298@go.buu.ac.th>
2025-01-16 19:02:47 +0700

    SP3

--- script.js
+++ script.js
@@ -10,23 +10,8 @@ document.querySelectorAll('.option').forEach(button => {
             feedback.style.color = "green";
             nextButton.style.display = "block"; // แสดงปุ่ม Next
             // เช็คว่ากำลังอยู่ในหน้าพี่หมี, พี่สิงโต, หรือหน้าที่ 3
-            if (window.location.pathname.includes('SolveProblems1')) {
-                document.body.style.backgroundImage = "url('background/SolveProblems1.png')";
-            } else if (window.location.pathname.includes('SolveProblems2')) {
-                document.body.style.backgroundImage = "url('background/SolveProblems2.png')";
-            } else if (window.location.pathname.includes('SolveProblems3')) {
-                document.body.style.backgroundImage = "url('background/SolveProblems3.png')";
-            }
-        } else {
-            feedback.textContent = "แย่จัง! ช่วยเลือกอีกครั้งนะ!";
-            feedback.style.color = "red";
-            nextButton.style.display = "none"; // ซ่อนปุ่ม Next
-            // ใช้พื้นหลังที่ต้องการเมื่อไม่ถูกต้อง
-            document.body.style.backgroundImage = "url('background/SolveProblems1.png')";
         }
     });
 });
 
-document.getElementById('next-button').addEventListener('click', () => {
-    window.location.href = "NextSP012.html";  // ลิงก์ไปที่หน้าถัดไป
-});
+
